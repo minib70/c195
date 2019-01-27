9a9f3afd236913e42562d1b63270d61956b5b5a3
taylor <taylor@tmtech.me>
2019-01-27 15:48:38 -0700

    Basic password handling working

---
+++ src/C195/Model/Validation.java
@@ -0,0 +1,11 @@
+/*
+ * Author: Taylor Vories
+ * WGU C195 Project
+ * This class provides methods shared across the controllers to validate user input
+ */
+
+package C195.Model;
+
+public class Validation {
+
+}
--- src/C195/View_Controller/LoginController.java
+++ src/C195/View_Controller/LoginController.java
@@ -1,6 +1,9 @@
 package C195.View_Controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
@@ -8,9 +11,29 @@ import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
     private VBox rootLayout;
+    @FXML private TextField textFieldUsername, textFieldPassword;
 
     public LoginController(VBox rootLayout) {
         this.rootLayout = rootLayout;
+    }
+
+    public void onEnter() { //TODO: Really set up password stuff.
+        String username = textFieldUsername.getText();
+        String password = textFieldPassword.getText();
+
+        if(password.equals("password")) { // Password is correct
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Login Successful!");
+            alert.setHeaderText("Well done.  You guessed the password!");
+            alert.setContentText("Password really is a good password.");
+            alert.showAndWait();
+        } else  {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Login FAILED!");
+            alert.setHeaderText("HALT!");
+            alert.setContentText("Are you a hacker?");
+            alert.showAndWait();
+        }
     }
 
     @Override
