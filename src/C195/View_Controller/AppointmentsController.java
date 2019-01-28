703ff5e086b119192976af61a6f564def9bf5c2e
taylor <taylor@tmtech.me>
2019-01-27 21:52:35 -0700

    Adding Appointments view.  Running into issue with rootlayout

---
+++ src/C195/View_Controller/AppointmentsController.java
@@ -0,0 +1,20 @@
+package C195.View_Controller;
+
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AppointmentsController implements Initializable {
+    private VBox rootLayout;
+
+    public AppointmentsController(VBox rootLayout) {
+        this.rootLayout = rootLayout;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+}
--- src/C195/View_Controller/LoginController.java
+++ src/C195/View_Controller/LoginController.java
@@ -1,11 +1,15 @@
 package C195.View_Controller;
 
+import C195.C195;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -18,7 +22,7 @@ public class LoginController implements Initializable {
         this.rootLayout = rootLayout;
     }
 
-    public void onEnter() { //TODO: Really set up password stuff.
+    public void onEnter() throws IOException { //TODO: Really set up password stuff.
         String username = textFieldUsername.getText();
         String password = textFieldPassword.getText();
 
@@ -28,6 +32,7 @@ public class LoginController implements Initializable {
             alert.setHeaderText("Well done.  You guessed the password!");
             alert.setContentText("Password really is a good password.");
             alert.showAndWait();
+            showAppointments();
         } else  {
             Alert alert = new Alert(Alert.AlertType.WARNING);
             alert.setTitle("Login FAILED!");
@@ -35,6 +40,18 @@ public class LoginController implements Initializable {
             alert.setContentText("Are you a hacker?");
             alert.showAndWait();
         }
+    }
+
+    public void showAppointments() throws IOException {
+        // Instantiate the controller
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(C195.class.getResource("View_Controller/Appointments.fxml"));
+        AppointmentsController controller = new AppointmentsController(rootLayout);
+        loader.setController(controller);
+
+        AnchorPane appointments = loader.load();
+        rootLayout.getChildren().add(appointments);
+        //rootLayout.set
     }
 
     @Override
