0284ef3375b67bf880ef4d64aeb9153abd48668a
taylor <taylor@tmtech.me>
2019-02-06 21:10:43 -0700

    Got appointments to show up.  Need to change monthly and weekly

--- src/C195/View_Controller/AppointmentsController.java
+++ src/C195/View_Controller/AppointmentsController.java
@@ -4,11 +4,14 @@ import C195.C195;
 import C195.Model.Appointment;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.sql.PreparedStatement;
@@ -21,29 +24,52 @@ public class AppointmentsController implements Initializable {
     private ObservableList<Appointment> appointments;
     @FXML private Label labelTitle;
     @FXML private TableView<Appointment> tableViewMonthly;
+    @FXML private TableColumn<Appointment, String> columnMonthlyTitle;
+    @FXML private TableColumn<Appointment, String> columnMonthlyDescription;
+    @FXML private TableColumn<Appointment, String> columnMonthlyContact;
+    @FXML private TableColumn<Appointment, String> columnMonthlyLocation;
+    @FXML private TableColumn<Appointment, String> columnMonthlyStart;
+    @FXML private TableColumn<Appointment, String> columnMonthlyEnd;
 
     public AppointmentsController(C195 main) {
         this.main = main;
+        this.appointments = FXCollections.observableArrayList();
+    }
+
+    private void showMonthlyApptData(ObservableList<Appointment> appointments) {
+        FilteredList<Appointment> filteredAppointments = new FilteredList<>(appointments, p -> true);
+        //todo: add search?
+
+        // Wrap filtered list in sorted list
+        SortedList<Appointment> sortedAppointments = new SortedList<>(filteredAppointments);
+
+        // Bind sorted list to TableVIew
+        sortedAppointments.comparatorProperty().bind(tableViewMonthly.comparatorProperty());
+        columnMonthlyTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
+        columnMonthlyDescription.setCellValueFactory(new PropertyValueFactory<>("description"));
+        columnMonthlyContact.setCellValueFactory(new PropertyValueFactory<>("contact"));
+        columnMonthlyLocation.setCellValueFactory(new PropertyValueFactory<>("location"));
+        columnMonthlyStart.setCellValueFactory(new PropertyValueFactory<>("start"));
+        columnMonthlyEnd.setCellValueFactory(new PropertyValueFactory<>("end"));
+        tableViewMonthly.setItems(sortedAppointments);
+        tableViewMonthly.refresh();
     }
 
     private void loadAppointments() {
-        appointments = FXCollections.observableArrayList();
-
         try {
             PreparedStatement stmt = C195.dbConnection.prepareStatement(
                     "SELECT appointment.appointmentId, appointment.customerId, appointment.title, "
                     + "appointment.description, appointment.start, appointment.end, appointment.createdBy, "
-                    + "customer.customerName, customer.customerId "
-                    + "FROM appointment,customer "
-                    + "WHERE appointment.customerId = customer.customerId AND appointment.createdBy = ? "
+                    + "appointment.location, appointment.contact, customer.customerName, customer.customerId "
+                    + "FROM appointment, customer "
+                    + "WHERE appointment.customerId = customer.customerId "
                     + "ORDER BY start"
             );
-            stmt.setString(1, main.currentUser.getUsername());
             ResultSet rs = stmt.executeQuery();
 
             while(rs.next()) {
                 Appointment appt = new Appointment();
-                appt.setAppointmentID(rs.getInt("appointment,appointmentId"));
+                appt.setAppointmentID(rs.getInt("appointment.appointmentId"));
                 appt.setCustomerID(rs.getInt("appointment.customerId"));
                 appt.setTitle(rs.getString("appointment.title"));
                 appt.setDescription(rs.getString("appointment.description"));
@@ -53,8 +79,7 @@ public class AppointmentsController implements Initializable {
                 appt.setEnd(rs.getString("appointment.end"));
                 appointments.add(appt);
             }
-
-            tableViewMonthly.getItems().setAll(appointments);
+            showMonthlyApptData(appointments);
 
         } catch(SQLException e){
             System.out.println("Issue with SQL");
