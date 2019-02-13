04e227b7a02343213dfb564e0ed99e87e963b0b6
taylor <taylor@tmtech.me>
2019-02-13 15:30:51 -0700

    Types are populating

--- src/C195/View_Controller/AppointmentAddController.java
+++ src/C195/View_Controller/AppointmentAddController.java
@@ -9,6 +9,8 @@ import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -17,6 +19,9 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.ResourceBundle;
 
 public class AppointmentAddController implements Initializable {
@@ -24,14 +29,35 @@ public class AppointmentAddController implements Initializable {
     private ObservableList<Customer> customers;
     @FXML private TableView<Customer> tableViewCustomers;
     @FXML private TableColumn<Customer, String> tableColumnCustomerName;
+    @FXML private ComboBox<String> comboBoxStartTime, comboBoxEndTime, comboBoxType;
+    private final ObservableList<String> startTimes, endTimes, appointmentTypes;
+    private final DateTimeFormatter timeFormatter = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT);
+    private final DateTimeFormatter dateFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
+    @FXML private DatePicker datePicker;
+
 
     public AppointmentAddController(C195 main) {
         this.main = main;
         customers = FXCollections.observableArrayList();
+        startTimes = FXCollections.observableArrayList();
+        endTimes = FXCollections.observableArrayList();
+        appointmentTypes = FXCollections.observableArrayList();
     }
 
     @FXML public void buttonCancelClicked() throws IOException {
         main.showAppointmentsScreen();
+    }
+
+    public void populateTimes() {
+        // Set up start times
+        LocalTime time = LocalTime.of(8, 0);
+        do {
+            startTimes.add(time.format(timeFormatter));
+            endTimes.add(time.format(timeFormatter));
+            time = time.plusMinutes(15);
+        } while(!time.equals(LocalTime.of(17, 15)));
+        comboBoxStartTime.setItems(startTimes);
+        comboBoxEndTime.setItems(endTimes);
     }
 
     @SuppressWarnings("Duplicates")
@@ -49,6 +75,17 @@ public class AppointmentAddController implements Initializable {
         }
     }
 
+    private void populateAppointmentTypes() {
+        String[] types = {
+                "New Customer Introduction",
+                "Insider Trading Meeting",
+                "Team Gossip Session",
+                "Blame Assignment"
+        };
+        appointmentTypes.addAll(types);
+        comboBoxType.setItems(appointmentTypes);
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // Initialize
@@ -56,5 +93,9 @@ public class AppointmentAddController implements Initializable {
 
         // Show customer table
         showCustomerDataTable();
+
+        populateTimes();
+
+        populateAppointmentTypes();
     }
 }
